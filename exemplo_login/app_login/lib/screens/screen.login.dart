4777d271615b985779f84597219fac55377971d8
Viniciusalmei <vihhalmeida12@gmail.com>
2026-04-29 15:13:09 -0300

    Exemplo de Login API

---
+++ exemplo_login/app_login/lib/screens/screen.login.dart
@@ -0,0 +1,65 @@
+import 'dart:convert';
+
+import 'package:app_login/screens/screens.home.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class TelaLogin extends StatefulWidget {
+  const TelaLogin({super.key});
+
+  @override
+  State<TelaLogin> createState() => _TelaLoginState();
+}
+
+class _TelaLoginState extends State<TelaLogin> {
+  TextEditingController nomeDigitado = TextEditingController();
+  TextEditingController senhaDigitada = TextEditingController();
+  
+  List usuarios = [];
+
+  @override
+  void initState(){
+    super.initState();
+    fazerGet();
+  } 
+
+  void fazerGet () async{
+    final respostaServidor = await http.get(Uri.parse("http://10.109.72.19:3000/users"));
+    if(respostaServidor.statusCode == 200){
+      final dados = jsonDecode(respostaServidor.body);
+    setState(() {
+      usuarios = dados;
+    });
+    }
+  }
+
+  void fazerLogin() async {
+    bool estaLogado = false;
+    for(final usuario in usuarios){
+      if(usuario["nome"] == nomeDigitado.text && usuario["senha"] == senhaDigitada.text ){
+        estaLogado = true;
+        Navigator.push(context,MaterialPageRoute(builder: (context)=> TelaHome()));
+      }
+    }
+    if(estaLogado == false){
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Dados incorretos, tente novamente!")));
+    }
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title:Text("App Login")),
+      body: Center(child: 
+      Column(
+        children: [
+          Icon(Icons.person,size: 100),
+          TextField(controller: nomeDigitado,),
+          TextField(controller: senhaDigitada),
+          TextButton(onPressed: fazerLogin, child: Text("Logar"))
+        ],
+      )),
+    );
+  }
+}
---
+++ exemplo_login/app_login/lib/screens/screens.home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TelaHome extends StatefulWidget {
+  const TelaHome({super.key});
+
+  @override
+  State<TelaHome> createState() => _TelaHomeState();
+}
+
+class _TelaHomeState extends State<TelaHome> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
